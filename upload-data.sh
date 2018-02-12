c970d62dec11aef089ecea06741e11f3e2484281
ycyr <1794646+ycyr@users.noreply.github.com>
2018-02-11 23:56:59 -0500

    Update upload-data.sh

--- upload-data.sh
+++ upload-data.sh
@@ -2,8 +2,8 @@
 cd /root/elk-stack-docker-pwd/
 pip install elasticsearch-curator
 docker-compose up -d
-sleep 30
-curator --config curator.yml create-index.yml
+sleep 60
+/usr/bin/curator --config /root/elk-stack-docker-pwd/curator.yml /root/elk-stack-docker-pwd/create-index.yml
 gunzip logs.jsonl.gz
 curl -H 'Content-Type: application/x-ndjson' -XPOST 'localhost:9200/_bulk?pretty' --data-binary @logs.jsonl
 curl -XGET 'localhost:9200/_cat/indices?v&pretty'
